52fa97dc6b73b9c62e8d67a66f75bad9879b242b
dragonslaya <dragonslaya.ss@gmail.com>
2021-11-16 17:32:53 -0500

    weaver not runs on lud.dll
    Signed-off-by: dragonslaya <dragonslaya.ss@gmail.com>

--- LUDServer/LUD/LUDServer/Program.cs
+++ LUDServer/LUD/LUDServer/Program.cs
@@ -1,10 +1,15 @@
 using System;
 using LUD.Authenticators;
 using LUD.Core;
+using LUD.Misc;
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(Title.Logo);
+Console.ForegroundColor = ConsoleColor.White;
 
 Server _masterServer;
 
-_masterServer = new(new RegionAuthenticator());
+_masterServer = new Server(new RegionAuthenticator());
 
 AppDomain.CurrentDomain.ProcessExit += OnAppClose;
 
